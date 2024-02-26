f4b3403be9c68652107e4e6ab0970964c79b3eed
Howard Dao <howard.dao@gmail.com>
2024-02-26 03:55:25 -0500

    adding functions to Keysight Lightwave, testing needed

--- MeasurementSystems/Keysight.py
+++ MeasurementSystems/Keysight.py
@@ -5,25 +5,71 @@ Author(s): Howard Dao
 
 import pyvisa as visa
 
-class KeysightLightwave(visa.resources.GPIBInstrument):
-    def __init__(self, visa):
-        self.idn = None
-        self.visa = None
+class Keysight8164B(visa.resources.GPIBInstrument):
+    def __init__(self, visa, wl_unit='m', pow_unit='mW'):
+        self.idn = visa.query('*IDN?')
+        self.idn = self.idn.split(',')
+        if self.idn[0] != 'Keysight Technologies' and self.idn[1] != '8164B':
+            print('Device not recognized as Keysight 8164B.')
+        self.visa = visa
 
-class Keysight81689A(KeysightLightwave):
+        self.wl_unit = wl_unit
+        self.pow_unit = pow_unit
+
+class Keysight81689A(Keysight8164B):
     """
     Tunable laser
     """
     def __init__(self, visa, slot:int):
         super().__init__(visa)
-        self.idn = visa.query('*IDN?')
+        self.idn2 = visa.query(f':slot{slot}:idn?')
+        self.idn2 = self.idn2.split(',')
+        if self.idn2[0] != 'Keysight Technologies' and self.idn2[1] != '81689A':
+            print('Slot module not recognized as Keysight 81689A.')
         self.visa = visa
         self.slot = slot
 
-    def set_power(self, pow:float, unit='mW'):
-        self.visa.write(f'sour{self.slot}:pow {pow}{unit}')
+    def get_wavelength(self):
+        """
+        Returns the laser wavelength.
+        """
+        lam = self.visa.query_ascii_values(f':sour{self.slot}:wav?')[0]
+        return lam
+    
+    def set_wavelength(self, lam:float):
+        """
+        Sets the laser wavelength in the previously specified unit.
 
-class Keysight81636B(KeysightLightwave):
+        Parameters:
+            lam : float
+                Laser wavelength in meters.
+        """
+        self.visa.write(f':sour{self.slot}:wav {lam}{self.wl_unit}')
+
+    def get_power(self):
+        """
+        Returns the laser output power.
+        """
+        pow = self.visa.query_ascii_values(f':sour{self.slot}:pow?')[0]
+        return pow
+    
+    def set_power(self, pow:float):
+        """
+        Sets the laser output power in the previously specified unit.
+
+        Parameters:
+            pow : float
+                Laser output power.
+        """
+        self.visa.write(f'sour{self.slot}:pow {pow}{self.pow_unit}')
+
+    def output_off(self):
+        self.visa.write(f':sour{self.slot}:pow:stat 0')
+
+    def output_on(self):
+        self.visa.write(f':sour{self.slot}:pow:stat 1')
+
+class Keysight81634B(Keysight8164B):
     """
     Power sensor
     """
@@ -32,7 +78,11 @@ class Keysight81636B(KeysightLightwave):
         self.visa = visa
         self.slot = slot
 
-class Keysight81634B(KeysightLightwave):
+    def meas_pow(self):
+        pow = self.visa.query_ascii_values(f':read{self.slot}:pow?')[0]
+        return pow
+
+class Keysight81636B(Keysight8164B):
     """
     Power sensor
     """
@@ -41,7 +91,7 @@ class Keysight81634B(KeysightLightwave):
         self.visa = visa
         self.slot = slot
 
-class Keysight81595B(KeysightLightwave):
+class Keysight81595B(Keysight8164B):
     """
     Switch
     """
